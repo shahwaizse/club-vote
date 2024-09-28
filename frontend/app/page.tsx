3f33adefbdf82d062d9ac5f018d18c951216f116
Muhammad Shahwaiz <mshahwaiz12@gmail.com>
2024-09-28 21:25:43 +0500

    button linked to dashboard

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,6 +3,7 @@
 import Navbar from "@/components/Navbar"
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -12,8 +13,12 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center">
           <div className="mb-5 text-lg w-fit rounded-full bg-black shadow-[0_0_5px_rgba(255,255,255,0.8)] antialiased py-1 px-3">Alpha v0.1</div>
           <Image src='/clubvote-transparent.png' alt="logo" width={500} height={500} />
-          <div className="text-2xl pl-7">a voting platform powered by web3</div>
-          <Button className="ml-3 mt-20 rounded-sm bg-white text-black font-oswald text-2xl py-1 px-3 hover:bg-salmon">Get Started</Button>
+          <div className="text-xl pl-7">a voting platform powered by web3</div>
+          <Link href="/user">
+            <Button className="ml-3 mt-20 rounded-sm bg-white text-black font-oswald text-2xl py-1 px-3 hover:bg-salmon">
+              Get Started
+            </Button>
+          </Link>
         </div>
       </div>
       <div className="font-bold p-5 bg-darkgrey font-oswald text-white text-2xl flex flex-row justify-around items-center">
